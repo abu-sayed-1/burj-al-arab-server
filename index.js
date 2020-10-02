e8102081cb0a8eac2f30d6b8104e43ff24aa0d9d
as ahmed <rakibasrr13@gmail.com>
2020-10-02 21:10:22 +0300

    burj-al-arab-server

--- index.js
+++ index.js
@@ -71,10 +71,9 @@ client.connect(err => {
   })
 
 });
-
+// deploy ^ korar somoy ei code gula^ li khte hobe
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(process.env.PORT || port)
-// deploy ^ korar somoy ei code gula^ li khte hobe
